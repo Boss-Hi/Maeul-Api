762a15a28524bff1cbcaf0c903dd9714e62b69bd
정태현 JEONG TAEHYUN <dev9163@commerceon.co.kr>
2026-08-17 22:32:47 +0900

    init

---
+++ src/main/java/com/bosshi/maeul/MaeulApplication.java
@@ -0,0 +1,13 @@
+package com.bosshi.maeul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaeulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaeulApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bosshi/maeul/MaeulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bosshi.maeul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaeulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
