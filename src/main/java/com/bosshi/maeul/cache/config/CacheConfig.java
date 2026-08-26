0d161f44401f9937b59620bb43f3cfcd76baf4df
allen <zerad3208@gmail.com>
2026-08-26 19:20:54 +0900

    cache 설정 추가

---
+++ src/main/java/com/bosshi/maeul/cache/config/CacheConfig.java
@@ -0,0 +1,9 @@
+package com.bosshi.maeul.cache.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching // 캐시 기능 활성화
+public class CacheConfig {
+}
