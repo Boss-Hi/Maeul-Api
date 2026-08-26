af1a827c045f7a332a6b9469b75d6eac4f808756
allen <zerad3208@gmail.com>
2026-08-26 22:51:30 +0900

    모니터링 오류 수정

--- src/main/java/com/bosshi/maeul/common/security/SecurityConfig.java
+++ src/main/java/com/bosshi/maeul/common/security/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/actuator/**").permitAll()
                         .requestMatchers("/", "/index.html", "/favicon.ico", "/error", "/api/auth/**", "/api/open/**").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
