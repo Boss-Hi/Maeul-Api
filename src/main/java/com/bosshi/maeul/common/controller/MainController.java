105f4e13c2f09367d13930613c30a762c32d3a79
allen <zerad3208@gmail.com>
2026-08-29 17:10:39 +0900

    배포 스크립트 수정

--- src/main/java/com/bosshi/maeul/common/controller/MainController.java
+++ src/main/java/com/bosshi/maeul/common/controller/MainController.java
@@ -13,6 +13,6 @@ public class MainController {
     @GetMapping("/")
     public String index() {
         log.info("Accessed the index endpoint");
-        return "Welcome to the Maeul API V0.0.2";
+        return "Welcome to the Maeul API V0.0.3";
     }
 }
