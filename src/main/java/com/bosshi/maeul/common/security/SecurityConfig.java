572201efff7892ba59b1d2bb386279f5c162b7b1
allen <zerad3208@gmail.com>
2026-08-29 15:18:50 +0900

    test

--- src/main/java/com/bosshi/maeul/common/security/SecurityConfig.java
+++ src/main/java/com/bosshi/maeul/common/security/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/actuator/**").permitAll()
-                        .requestMatchers("/", "/index.html", "/favicon.ico", "/error", "/api/auth/**", "/api/open/**").permitAll()
+                        .requestMatchers("/", "/error", "/api/auth/**", "/api/open/**").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
