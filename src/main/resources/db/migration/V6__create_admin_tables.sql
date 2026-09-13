bef73ca517d8d145711ddccd93387f64bc715ce8
allen <zerad3208@gmail.com>
2026-09-13 23:42:34 +0900

    Create V6__create_admin_tables.sql

---
+++ src/main/resources/db/migration/V6__create_admin_tables.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS event_publication
+(
+    id               BINARY(16)    NOT NULL,
+    completion_date  DATETIME(6),
+    event_type       VARCHAR(512)  NOT NULL,
+    listener_id      VARCHAR(512)  NOT NULL,
+    publication_date DATETIME(6)   NOT NULL,
+    serialized_event VARCHAR(4000) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
